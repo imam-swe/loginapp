736e090abe962e355794b7df1caeb6745eb07533
Imam Hasan <imam.hasan.diu@gmail.com>
2021-07-02 00:18:06 +0600

    Add HTTP Request & API connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:login_module/pages/login_page.dart';
+
+import 'pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Screen',
+      title: 'Image Loader',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
+        primaryColor: Colors.white,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          elevation: 0,
+          foregroundColor: Colors.white,
+        ),
+        accentColor: Colors.redAccent,
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 22.0, color: Colors.redAccent),
+          headline2: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.w700,
+            color: Colors.redAccent,
+          ),
+          bodyText1: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.w400,
+            color: Colors.blueAccent,
+          ),
+        ),
       ),
       home: LoginPage(),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_http_post_request/api/api_service.dart';
+import 'package:flutter_http_post_request/model/login_model.dart';
+
+import '../ProgressHUD.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,11 +11,28 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool hidePassword = true;
+  bool isApiCallProcess = false;
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+  LoginRequestModel loginRequestModel;
   final scaffoldKey = GlobalKey<ScaffoldState>();
-  GlobalKey<FormState> globalFormKey = new GlobalKey();
+  @override
+  void initState() {
+    super.initState();
+    loginRequestModel = new LoginRequestModel();
+  }
 
   @override
   Widget build(BuildContext context) {
+    return ProgressHUD(
+      child: _uiSetup(context),
+      inAsyncCall: isApiCallProcess,
+      opacity: 0.3,
+      key: null,
+    );
+  }
+
+  Widget _uiSetup(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       backgroundColor: Theme.of(context).accentColor,
@@ -18,7 +40,7 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: <Widget>[
             Stack(
-              children: [
+              children: <Widget>[
                 Container(
                   width: double.infinity,
                   padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
@@ -28,32 +50,146 @@ class _LoginPageState extends State<LoginPage> {
                     color: Theme.of(context).primaryColor,
                     boxShadow: [
                       BoxShadow(
-                        color: Theme.of(context).hintColor.withOpacity(0.2),
-                        offset: Offset(0, 10),
-                        blurRadius: 20,
-                      )
+                          color: Theme.of(context).hintColor.withOpacity(0.2),
+                          offset: Offset(0, 10),
+                          blurRadius: 20)
                     ],
                   ),
                   child: Form(
                     key: globalFormKey,
                     child: Column(
-                      children: [
-                        SizedBox(
-                          height: 25,
-                        ),
+                      children: <Widget>[
+                        SizedBox(height: 25),
                         Text(
                           "Login",
                           style: Theme.of(context).textTheme.headline2,
-                        )
+                        ),
+                        SizedBox(height: 20),
+                        new TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          onSaved: (input) => loginRequestModel.email = input,
+                          validator: (input) => !input.contains('@')
+                              ? "Email Id should be valid"
+                              : null,
+                          decoration: new InputDecoration(
+                            hintText: "Email Address",
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context)
+                                        .accentColor
+                                        .withOpacity(0.2))),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).accentColor)),
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        new TextFormField(
+                          style:
+                              TextStyle(color: Theme.of(context).accentColor),
+                          keyboardType: TextInputType.text,
+                          onSaved: (input) =>
+                              loginRequestModel.password = input,
+                          validator: (input) => input.length < 3
+                              ? "Password should be more than 3 characters"
+                              : null,
+                          obscureText: hidePassword,
+                          decoration: new InputDecoration(
+                            hintText: "Password",
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context)
+                                        .accentColor
+                                        .withOpacity(0.2))),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).accentColor)),
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Theme.of(context).accentColor,
+                            ),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  hidePassword = !hidePassword;
+                                });
+                              },
+                              color: Theme.of(context)
+                                  .accentColor
+                                  .withOpacity(0.4),
+                              icon: Icon(hidePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        // ignore: deprecated_member_use
+                        FlatButton(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 80),
+                          onPressed: () {
+                            if (validateAndSave()) {
+                              print(loginRequestModel.toJson());
+
+                              setState(() {
+                                isApiCallProcess = true;
+                              });
+
+                              APIService apiService = new APIService();
+                              apiService.login(loginRequestModel).then((value) {
+                                if (value != null) {
+                                  setState(() {
+                                    isApiCallProcess = false;
+                                  });
+
+                                  if (value.token.isNotEmpty) {
+                                    final snackBar = SnackBar(
+                                        content: Text("Login Successful"));
+                                    scaffoldKey.currentState
+                                        // ignore: deprecated_member_use
+                                        .showSnackBar(snackBar);
+                                  } else {
+                                    final snackBar =
+                                        SnackBar(content: Text(value.error));
+                                    scaffoldKey.currentState
+                                        // ignore: deprecated_member_use
+                                        .showSnackBar(snackBar);
+                                  }
+                                }
+                              });
+                            }
+                          },
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Theme.of(context).accentColor,
+                          shape: StadiumBorder(),
+                        ),
+                        SizedBox(height: 15),
                       ],
                     ),
                   ),
-                )
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
     );
+  }
+
+  bool validateAndSave() {
+    final form = globalFormKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
   }
 }
